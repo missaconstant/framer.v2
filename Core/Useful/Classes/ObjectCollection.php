d503ece55f20d8759a0c997a34242b35254f2c90
Missa Constant <missabaihconstant@gmail.com>
2021-03-12 11:52:40 +0000

    Adding toArray param to get method of BaseModel

--- Core/Useful/Classes/ObjectCollection.php
+++ Core/Useful/Classes/ObjectCollection.php
@@ -12,16 +12,29 @@ class ObjectCollection extends \ArrayObject {
     /**
      * Convert collection to readable array
      * 
+     * @param $stdclassArray determine if should return an array of stdClassObject
+     * 
      * @return Array
      */
-    public function toArray() {
+    public function toArray($stdclassArray=false) {
         $nativeArray = new \ArrayObject;
 
         foreach ($this as $k => $object) {
-            $nativeArray->append( get_object_vars($object) );
+            $objVars = get_object_vars($object);
+            $nativeArray->append( $stdclassArray ? (object) $objVars : $objVars );
         }
 
         return $nativeArray->getArrayCopy();
+    }
+
+
+    /**
+     * Convert collection to readable stdClassObject array
+     * 
+     * @return Array[StdClass]
+     */
+    public function toObjArray() {
+        return $this->toArray(true);
     }
 
 }
